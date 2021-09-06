fb6d97b97b565850cad5acfec7311eca5c4bec5e
marianapatc <marianapatc@hotmail.com>
2021-09-06 20:46:13 +0100

    add padding fix

--- src/components/ui/layout/styles.js
+++ src/components/ui/layout/styles.js
@@ -1,15 +1,11 @@
 import styled from 'styled-components'
 
 export const StyledMain = styled.main`
-  padding-top: 3rem;
+  padding-top: 4rem;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
-    padding-top: 4rem;
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
     padding-top: 5rem;
-  }
+  }om 
 
   @media print {
     padding-top: 0;
