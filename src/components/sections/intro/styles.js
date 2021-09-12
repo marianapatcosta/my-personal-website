0f374959d9630644f608b695042a3cb32a752054
marianapatc <marianapatc@hotmail.com>
2021-09-12 21:37:30 +0100

    add some minor changes

--- src/components/sections/intro/styles.js
+++ src/components/sections/intro/styles.js
@@ -8,10 +8,10 @@ const TYPING_ANIMATION_DURATION_PER_CHARACTER = 0.15
 
 let vh
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && typeof document !== 'undefined') {
   vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
 }
-document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 const getBlinkingIterationCount = stringLength =>
   !!stringLength
