331b582d8118fab1214e9d244c13d02478819b2e
marianapatc <marianapatc@hotmail.com>
2021-09-13 19:44:04 +0100

    add style fix

--- src/components/sections/intro/styles.js
+++ src/components/sections/intro/styles.js
@@ -49,13 +49,13 @@ const waving = keyframes`
 
 export const StyledIntro = styled(StyledSection)`
   position: relative;
-  height: calc(100% - 7rem);
+  height: 100%;
   background-color: ${({ theme }) => theme.colors.highlight};
   box-shadow: inset 0 -1rem 0.7rem -1rem
     ${({ theme }) => theme.colors.highlightDark};
 
   @media (min-width: ${({ theme }) => theme.breakpoints.xs}) {
-    height: calc(100% - 9rem);
+    height: 100%;
   }
 
   @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
