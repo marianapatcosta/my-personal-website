73487a73d60fb3acd9d6154112707dfd93b5689d
marianapatc <marianapatc@hotmail.com>
2021-12-05 17:00:05 +0000

    fix intro height to occupy full screen in mobile devices

--- src/components/sections/intro/styles.js
+++ src/components/sections/intro/styles.js
@@ -49,6 +49,7 @@ const waving = keyframes`
 
 export const StyledIntro = styled(StyledSection)`
   position: relative;
+  height: calc(100vh - 7rem);
   background-color: ${({ theme }) => theme.colors.highlight};
   box-shadow: inset 0 -1rem 0.7rem -1rem
     ${({ theme }) => theme.colors.highlightDark};
